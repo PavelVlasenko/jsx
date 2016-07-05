566fb1b91705631c12e25178468743a65f625199
PavelVlasenko <vlasenko-pav@yandex.ru>
2016-07-05 12:56:55 +0700

    commit

--- src/main/java/jsx/Main.java
+++ src/main/java/jsx/Main.java
@@ -1,15 +1,26 @@
 package jsx;
 
 import jsx.core.AstParser;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 /**
  * @author Pavel Vlasenko
  */
 public class Main
 {
-    public static void main(String ... args)
+    public static void main(String ... args) throws Exception
     {
+        Path path = Paths.get(ClassLoader.getSystemResource("MyTest.java").toURI());
+        String source = Files.lines(path).collect(Collectors.joining());
+
         AstParser parser = new AstParser();
-       // parser.generateSourceCode(parser.generateAst());
+        ParserRuleContext tree = parser.generateAst(source);
+        String sourceCode = parser.generateSourceCode(tree);
+        System.out.println(sourceCode);
     }
 }
